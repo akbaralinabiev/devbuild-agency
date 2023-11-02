9c98fcedfea5f732d24819c7b29454ebf96aec89
akbaralinabiev <akbaralinabiev14@gmail.com>
2023-11-02 13:51:17 +0100

    updating

---
+++ src/sections/home/Home.jsx
@@ -0,0 +1,101 @@
+import React, { useState, useEffect, useRef } from "react";
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/icons/logo.png'
+import "./home.css";
+import "./custom.js"
+
+const Menu = ({ closeMenu }) => {
+  return (
+    <>
+      <div className="nav-links-mobile">
+        <ul>
+          <li>
+            <a href="#Services" onClick={closeMenu}>Services</a>
+            <a href="#Pricing" onClick={closeMenu}>Pricing</a>
+            <a href="#Case Study" onClick={closeMenu}>Case Study</a>
+            <a href="#About" onClick={closeMenu}>About</a>
+            <a href="#contact" onClick={closeMenu}>Contact</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+const Home = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const menuRef = useRef(null);
+
+  const closeMenu = () => {
+    setToggleMenu(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        closeMenu();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div id="home">
+      <div className="wrapper">
+        
+        <div className="navbar">
+          <div className="logo">
+            <img src={logo} alt="" />
+            <a href="#home"><h2>devsbuild.</h2></a>
+          </div>
+          <div className="nav-links">
+            <ul>
+              <li>
+                <a href="#Services">Services</a>
+                <a href="#Pricing">Pricing</a>
+                <a href="#Case Study">Case Study</a>
+                <a href="#About">About</a>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+            <button type="button" href="#contact" id="cta" className="nav-button">Let's Talk <span></span></button>
+          </div>
+          
+          <div className="navbar-menu" ref={menuRef}>
+            {toggleMenu ? (
+              <RiCloseLine
+                color="#fff"
+                size={27}
+                onClick={() => setToggleMenu(false)}
+              />
+            ) : (
+              <RiMenu3Line
+                color="#fff"
+                size={27}
+                onClick={() => setToggleMenu(true)}
+              />
+            )}
+            {toggleMenu && (
+              <div className="gpt3__navbar-menu_container scale-up-center">
+                <div className="gpt3__navbar-menu_container-links">
+                  <Menu closeMenu={closeMenu} />
+                </div>
+              </div>
+            )}
+          </div>
+        </div>    
+        <div className="title">
+            <h3>We build digital products, brands, and experience.</h3>
+            <p>Building excitement around your vision and accelerating your growth</p>
+          </div>    
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/sections/home/custom.js
@@ -0,0 +1,22 @@
+(function() {
+    const buttons = document.querySelectorAll("#cta");
+  
+    buttons.forEach(button => {
+      ["mouseenter", "mouseout"].forEach(evt => {
+        button.addEventListener(evt, e => {
+          let parentOffset = button.getBoundingClientRect(),
+              relX = e.pageX - parentOffset.left,
+              relY = e.pageY - parentOffset.top;
+  
+          const span = button.getElementsByTagName("span");
+  
+          span[0].style.top = relY + "px";
+          span[0].style.left = relX + "px";
+        });
+      });
+    });
+  })();
+
+
+  
+ 
